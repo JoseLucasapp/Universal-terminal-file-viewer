2de9130263197144b10212b9dfb9fb8e2f864e0c
José Lucas <josluc2016@gmail.com>
2025-05-17 23:55:17 -0300

    feat: generate image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use image::GenericImageView;
+
+fn main(){
+    let img = image::open("teste.jpg").unwrap().grayscale();
+    let (width, height) = img.dimensions();
+
+    let chars = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."];
+
+    for y in (0..height).step_by(6){
+        for x in (0..width).step_by(3){
+            let pixel = img.get_pixel(x,y);
+            let detax = pixel[0] as f32;
+            let index = (detax / 255.0 * (chars.len() - 1) as f32).round() as usize;
+            print!("{}", chars[index]);
+        }
+        println!();
+    }
 }
