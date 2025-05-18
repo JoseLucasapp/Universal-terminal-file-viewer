36ec47e8a8230354d55ca1b2094e02e35024a03c
José Lucas <josluc2016@gmail.com>
2025-05-18 15:05:09 -0300

    fix: prettify pdf

--- src/main.rs
+++ src/main.rs
@@ -101,9 +101,21 @@ fn show_pdf(path: &str){
     match pdf_extract::extract_text(path){
         Ok(text)=>{
             println!("--- PDF Content ---\n");
-            for line in text.lines(){
-                println!("{}", line);
-            }
+
+            let clean = text
+                .lines()
+                .map(|line| line.trim())
+                .filter(|line| !line.is_empty())
+                .collect::<Vec<_>>()
+                .join(" ");
+
+            let pretty = clean
+            .replace(". ", ".\n\n")
+            .replace("! ", "!\n")
+            .replace("? ", "?\n");
+
+
+            println!("{}", pretty)
         }
         Err(e)=>{
             eprintln!("Failed to open pdf: {}", e);
