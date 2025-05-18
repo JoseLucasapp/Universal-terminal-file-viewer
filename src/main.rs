cd07f174f06f3138e7d69c01e4032408e200aaae
José Lucas <josluc2016@gmail.com>
2025-05-17 23:55:55 -0300

    feat: generate image with colors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use image::GenericImageView;
+use crossterm::style::{Stylize, PrintStyledContent};
+use std::io::{stdout};
+use crossterm::{execute, terminal};
 
-fn main(){
-    let img = image::open("teste.jpg").unwrap().grayscale();
-    let (width, height) = img.dimensions();
+fn main() {
+    let img = image::open("teste.jpg").unwrap();
+    let resized = img.resize(80, 40, image::imageops::FilterType::Nearest);
 
-    let chars = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."];
+    let mut stdout = stdout();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
 
-    for y in (0..height).step_by(6){
-        for x in (0..width).step_by(3){
-            let pixel = img.get_pixel(x,y);
-            let detax = pixel[0] as f32;
-            let index = (detax / 255.0 * (chars.len() - 1) as f32).round() as usize;
-            print!("{}", chars[index]);
+    for y in 0..resized.height(){
+        for x in 0..resized.width(){
+            let pixel = resized.get_pixel(x,y);
+            let r = pixel[0];
+            let g = pixel[1];
+            let b = pixel[2];
+
+            let style = "█".with(crossterm::style::Color::Rgb {r,g,b});
+            execute!(stdout, PrintStyledContent(style)).unwrap();
         }
+
         println!();
     }
 }
