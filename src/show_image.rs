677192bcb02d3a3c541faa13391e84870df2f9a2
José Lucas <josluc2016@gmail.com>
2025-05-22 13:55:42 -0300

    feat: move show_image to a new file

---
+++ src/show_image.rs
@@ -0,0 +1,35 @@
+use image::GenericImageView;
+use std::io::{stdout};
+use crossterm::style::{Stylize, PrintStyledContent};
+use crossterm::{execute, terminal, cursor};
+
+pub fn main(path: &str, width_scale: u32, height_scale: u32){
+    let img = match image::open(path){
+        Ok(img) => img,
+        Err(e)=>{
+            eprintln!("Failed to open image: {}", e);
+            std::process::exit(1);
+        }
+    };
+    let (_w, h) = img.dimensions();
+    let new_height = ((h as f32 * 0.5) / (10.0 / height_scale.max(1) as f32)) as u32;
+    let resized = img.resize(80, new_height, image::imageops::FilterType::Nearest);
+
+    let mut stdout = stdout();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0, 0)).unwrap();
+
+    for y in 0..resized.height(){
+        for x in 0..resized.width(){
+            let pixel = resized.get_pixel(x,y);
+            let r = pixel[0];
+            let g = pixel[1];
+            let b = pixel[2];
+
+            let block = "█".repeat(width_scale as usize);
+            let style = block.with(crossterm::style::Color::Rgb {r,g,b});
+            execute!(stdout, PrintStyledContent(style)).unwrap();
+        }
+
+        println!();
+    }
+}
